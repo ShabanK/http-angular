b253f171310b0943bfaeb79129ee95ee5aa05faf
ShabanK <shabankhawar99@gmail.com>
2021-01-22 16:56:57 +0500

    can fetch data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from "@angular/common/http"
 
 import { AppComponent } from './app.component';
-
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { PageListComponent } from './page-list/page-list.component';
+import {WikipediaService} from "./wikipedia.service"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    PageListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
-  providers: [],
+  providers: [WikipediaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
